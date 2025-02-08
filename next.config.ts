5955187560ebe6445b5de0eb9dc8b101a779f7cb
Angel Gomez Garcia <toiletangel8@gmail.com>
2025-02-07 19:48:07 -0600

    s

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Permite exportar la app como un sitio estático
+  images: {
+    unoptimized: true, // Necesario porque GitHub Pages no soporta la optimización de imágenes de Next.js
+  },
+  basePath: "/cv_angel", // Ajusta según el nombre de tu repositorio
+  assetPrefix: "/cv_angel/",
 };
 
 export default nextConfig;
